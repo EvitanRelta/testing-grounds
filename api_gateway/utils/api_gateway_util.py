0aea440a9d0031f7cb20e065133ffc5103165afa
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-05 20:30:29 +0800

    format

--- api_gateway/utils/api_gateway_util.py
+++ api_gateway/utils/api_gateway_util.py
@@ -5,9 +5,13 @@ import websockets
 from fastapi import HTTPException, WebSocket
 from fastapi.responses import JSONResponse
 
-from .addresses import (API_PORT, MATCHING_SERVICE_HOST,
-                        QUESTIONS_SERVICE_HOST, SESSIONS_SERVICE_HOST,
-                        USERS_SERVICE_HOST)
+from .addresses import (
+    API_PORT,
+    MATCHING_SERVICE_HOST,
+    QUESTIONS_SERVICE_HOST,
+    SESSIONS_SERVICE_HOST,
+    USERS_SERVICE_HOST,
+)
 from .api_permissions import *
 
 
