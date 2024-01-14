1a4abb87fa0853b941bb35beef0692571ec22593
EvitanRelta <shauntanzongzhi@gmail.com>
2024-01-14 11:12:12 +0800

    Refactor to be closer to WraLU code

--- krelu.py
+++ krelu.py
@@ -1,3 +1,9 @@
+import itertools
+import multiprocessing
+import time
+
+import deepzono_nodes as dn
+import numpy as np
 from elina_scalar import *
 from elina_dimension import *
 from elina_linexpr0 import *
@@ -5,17 +11,11 @@ from elina_abstract0 import *
 from fppoly import *
 from fconv import *
 
-import numpy as np
-import time
-import itertools
-import multiprocessing
-import math
-
 from config import config
 
 
-def encode_kactivation_cons(nn, man, element, offset, layerno, length, lbi, ubi, constraint_groups, need_pop, domain, activation_type, K=3, s=-2, approx=True):
-    import deepzono_nodes as dn
+def encode_kactivation_cons(nn, man, element, offset, layerno, length, lbi, ubi, constraint_groups, need_pop, domain,
+                            activation_type, K=3, s=-2, approx=True):
     if need_pop:
         constraint_groups.pop()
 
@@ -28,9 +28,9 @@ def encode_kactivation_cons(nn, man, element, offset, layerno, length, lbi, ubi,
         kact_args = sparse_heuristic_curve(length, lbi, ubi, activation_type == "Sigmoid", s=s)
 
     kact_cons = []
-    tdim = ElinaDim(offset+length)
-    if domain == 'refinezono':
-        element = dn.add_dimensions(man,element,offset+length,1)
+    tdim = ElinaDim(offset + length)
+    if domain == "refinezono":
+        element = dn.add_dimensions(man, element, offset + length, 1)
 
     KAct.man = man
     KAct.element = element
@@ -41,63 +41,51 @@ def encode_kactivation_cons(nn, man, element, offset, layerno, length, lbi, ubi,
     KAct.domain = domain
     KAct.type = activation_type
 
-    start = time.time()
-
-    if domain == 'refinezono':
+    if domain == "refinezono":
         with multiprocessing.Pool(config.numproc) as pool:
             input_hrep_array = pool.map(get_ineqs_zono, kact_args)
+        with multiprocessing.Pool(config.numproc) as pool:
+            kact_results = pool.starmap(
+                make_kactivation_obj,
+                zip(input_hrep_array, [approx] * len(input_hrep_array)),
+            )
     else:
         total_size = 0
-        for varsid in kact_args:
-            size = 3**len(varsid) - 1
+        for var_ids in kact_args:
+            size = 3 ** len(var_ids) - 1
             total_size = total_size + size
 
         linexpr0 = elina_linexpr0_array_alloc(total_size)
         i = 0
-        for varsid in kact_args:
-            for coeffs in itertools.product([-1, 0, 1], repeat=len(varsid)):
+        for var_ids in kact_args:
+            for coeffs in itertools.product([-1, 0, 1], repeat=len(var_ids)):
                 if all(c == 0 for c in coeffs):
                     continue
+                linexpr0[i] = generate_linexpr0(offset, var_ids, coeffs)
+                i += 1
+        upper_bounds = get_upper_bound_for_linexpr0(man, element, linexpr0, total_size, layerno)
 
-                linexpr0[i] = generate_linexpr0(offset, varsid, coeffs)
-                i = i + 1
-        upper_bound = get_upper_bound_for_linexpr0(man,element,linexpr0, total_size, layerno)
-        i=0
+        i = 0
         input_hrep_array = []
-        for varsid in kact_args:
+        for var_ids in kact_args:
             input_hrep = []
-            for coeffs in itertools.product([-1, 0, 1], repeat=len(varsid)):
+            for coeffs in itertools.product([-1, 0, 1], repeat=len(var_ids)):
                 if all(c == 0 for c in coeffs):
                     continue
-                input_hrep.append([upper_bound[i]] + [-c for c in coeffs])
-                i = i + 1
+                input_hrep.append([upper_bounds[i]] + [-c for c in coeffs])
+                i += 1
             input_hrep_array.append(input_hrep)
-    end_input = time.time()
+        with multiprocessing.Pool(config.numproc) as pool:
+            kact_results = pool.starmap(
+                make_kactivation_obj,
+                zip(input_hrep_array, [approx] * len(input_hrep_array)),
+            )
 
-    # kact_results = list(map(make_kactivation_obj, input_hrep_array))
-
-    # end1 = time.time()
-
-    with multiprocessing.Pool(config.numproc) as pool:
-        # kact_results = pool.map(make_kactivation_obj, input_hrep_array)
-        kact_results = pool.starmap(make_kactivation_obj, zip(input_hrep_array, len(input_hrep_array) * [approx]))
-
-    # end2 = time.time()
-
-    # if end2-end_input>10:
-    #     print(f"list(map()) time: {end1-end_input:.3f}, multiprocessing time: {end2-end1:.3f}")
-
-    gid = 0
-    for inst in kact_results:
-        varsid = kact_args[gid]
-        inst.varsid = varsid
+    for gid, inst in enumerate(kact_results):
+        inst.varsid = kact_args[gid]
         kact_cons.append(inst)
-        gid = gid+1
-    end = time.time()
 
-    if config.debug:
-        print(f'total k-activation time: {end-start:.3f}. Time for input: {end_input-start:.3f}. Time for k-activation constraints {end-end_input:.3f}.')
-    if domain == 'refinezono':
-        element = dn.remove_dimensions(man, element, offset+length, 1)
+    if domain == "refinezono":
+        element = dn.remove_dimensions(man, element, offset + length, 1)
 
     constraint_groups.append(kact_cons)
